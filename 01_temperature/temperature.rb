19df1524c6c55b46f4b1c7427535de4e08f56f86
jameskritikos <james@localhost.localdomain>
2020-04-13 21:12:56 -0400

    New solutions

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
-#write your code here
+# frozen_string_literal: true
+
+def ftoc(f_degrees)
+  (f_degrees - 32) * 5.0 / 9.0
+end
+
+def ctof(c_degrees)
+  c_degrees * 9.0 / 5.0 + 32
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,26 @@
-#write your code here
+# frozen_string_literal: true
+
+def first_phoneme_index(word)
+  single_phonemes = ['qu']
+  result = word.index(/[aeiou]/)
+  return result if result.nil? || result == 0
+
+  result = word.index(/[aeiou]/, result + 1) until result.nil? || !single_phonemes.include?(word[result - 1..result])
+
+  result
+end
+
+def translate_word(word)
+  return '' if word.empty?
+
+  first_phoneme = first_phoneme_index(word)
+  return word + 'ay' if first_phoneme == 0
+
+  word[first_phoneme..-1] + word[0..first_phoneme - 1] + 'ay'
+end
+
+def translate(word)
+  words = word.split
+  words = words.map { |w| translate_word(w) }
+  words.join(' ')
+end
